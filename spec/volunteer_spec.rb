9e5f3834395b7a39bea96021a490f9c73a93a88e
ericschoettle <eric.schoettle@gmail.com>
2017-05-05 15:12:57 -0700

    refactor volunteer specs

--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -3,36 +3,31 @@ require 'spec_helper'
 describe(Volunteer) do
   describe '#name' do
     it('gives the name of the volunteer') do
-      test_volunteer = Volunteer.new({:name => "Toronto"})
-      expect(test_volunteer.name()).to eq("Toronto")
+      volunteer = Helper.make_volunteers()
+      expect(volunteer.name()).to eq("bob")
     end
   end
 
-  describe '#id and #save' do
-    it('saves and gives ID') do
-      test_volunteer = Volunteer.new({:name => "Toronto"})
-      test_volunteer.save()
-      expect(test_volunteer.id()).to be_an_instance_of(Fixnum)
-      expect(Volunteer.all()).to eq([test_volunteer])
+  describe '#id' do
+    it('gives ID') do
+      volunteer = Helper.make_volunteers()
+      expect(volunteer.id()).to be_an_instance_of(Fixnum)
+      expect(Volunteer.all()).to eq([volunteer])
     end
   end
 
   describe '#==' do
     it('is true if volunteers have same name') do
-      volunteer1 = Volunteer.new({:name => "Toronto"})
-      volunteer2 = Volunteer.new({:name => "Toronto"})
+      volunteer1 = Helper.make_volunteers()
+      volunteer2 = Helper.make_volunteers()
       expect(volunteer1).to eq(volunteer2)
     end
   end
 
   describe '#projects' do
     it('returns an array of projects for that volunteer') do
-      project = Project.new({:name => "polar express"})
-      project.save()
-      project1 = Project.new({:name => "pineapple express"})
-      project1.save()
-      volunteer = Volunteer.new({:name => "Toronto"})
-      volunteer.save()
+      project, project1 = Helper.make_projects(2)
+      volunteer = Helper.make_volunteers()
       volunteer.update_projects_volunteers ([project.id, project1.id])
       expect(volunteer.projects()).to eq([project, project1])
     end
@@ -40,16 +35,13 @@ describe(Volunteer) do
 
   describe '#delete_volunteer' do
     it "deletes a volunteer from the volunteers database" do
-      volunteer = Volunteer.new({:name => "Seattle"})
-      volunteer.save()
+      volunteer = Helper.make_volunteers()
       volunteer.delete_volunteer()
       expect(Volunteer.all()).to eq([])
     end
     it "deletes a volunteer from the projects_volunteers database" do
-      volunteer = Volunteer.new({:name => "Seattle"})
-      volunteer.save()
-      project = Project.new({:name => "polar express"})
-      project.save()
+      volunteer = Helper.make_volunteers()
+      project = Helper.make_projects()
       volunteer.update_projects_volunteers([project.id()])
       project_volunteer = Helper.all_projects_volunteers().first()
       expect(project_volunteer["volunteer_id"].to_i()).to eq(volunteer.id())
